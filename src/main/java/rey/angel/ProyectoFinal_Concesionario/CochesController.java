6c76ac9557cded404d4024e309f61cd23b8bf50e
Angel-Rey11 <91887811+Angel-Rey11@users.noreply.github.com>
2022-04-25 19:23:36 +0200

    Clases y FXML nuevos

--- src/main/java/rey/angel/ProyectoFinal_Concesionario/CochesController.java
+++ src/main/java/rey/angel/ProyectoFinal_Concesionario/CochesController.java
@@ -1,10 +1,47 @@
 package rey.angel.ProyectoFinal_Concesionario;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Optional;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextInputDialog;
+import javafx.stage.StageStyle;
+import model.Dao.CocheDao;
+import model.DataObject.Coche;
 
 public class CochesController {
+	
+	CocheDao cd = new CocheDao();
+	
+	@FXML
+	private TableView<Coche> tab;
+	
+	@FXML
+	private void ShowCars() throws IOException {
+		Collection<Coche> misCoches = cd.getAll();
+		for (Coche c: misCoches) {
+			tab.getItems().add(c);
+		}
+	}
+	
+	@FXML
+	private void ModifyCar() throws IOException {
+		TextInputDialog dialogo = new TextInputDialog();
+		dialogo.setTitle("Modificar Vehiculo");
+		dialogo.setHeaderText("Por favor, ingresa la matrícula del vehículo");
+		dialogo.setContentText("Introduzca la matrícula del cliente");
+		dialogo.initStyle(StageStyle.UTILITY);
+		Optional<String> respuesta = dialogo.showAndWait();
+		respuesta.ifPresent((string)->{
+			try {
+				App.setRoot("ModificaCoche");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+	}
 	
 	@FXML
     private void switchToInicio() throws IOException {
--- src/main/java/rey/angel/ProyectoFinal_Concesionario/InicioController.java
+++ src/main/java/rey/angel/ProyectoFinal_Concesionario/InicioController.java
@@ -1,9 +1,47 @@
 package rey.angel.ProyectoFinal_Concesionario;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Optional;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextInputDialog;
+import javafx.stage.StageStyle;
+import model.Dao.ClienteDao;
+import model.DataObject.Cliente;
 
 public class InicioController {
+	
+	ClienteDao cd = new ClienteDao();
+	
+	@FXML
+	private TableView<Cliente> tab;
+	
+	@FXML
+	private void ShowClients() throws IOException {
+		Collection<Cliente> misClientes = cd.getAll();
+		for (Cliente c: misClientes) {
+			tab.getItems().add(c);
+		}
+	}
+	
+	@FXML
+	private void ModifyClient() throws IOException {
+		TextInputDialog dialogo = new TextInputDialog();
+		dialogo.setTitle("Modificar Cliente");
+		dialogo.setHeaderText("Por favor, ingresa el DNI del cliente");
+		dialogo.setContentText("Introduzca el DNI del cliente");
+		dialogo.initStyle(StageStyle.UTILITY);
+		Optional<String> respuesta = dialogo.showAndWait();
+		respuesta.ifPresent((String)->{
+			try {
+				App.setRoot("ModificaCliente");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+	}
 
     @FXML
     private void switchToFormulario() throws IOException {
