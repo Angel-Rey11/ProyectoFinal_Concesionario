c3e0e0547f4bc06278cf5260db2eda6f7bc02308
Angel-Rey11 <91887811+Angel-Rey11@users.noreply.github.com>
2022-04-26 20:24:07 +0200

    Terminando tablas

--- src/main/java/rey/angel/ProyectoFinal_Concesionario/CochesController.java
+++ src/main/java/rey/angel/ProyectoFinal_Concesionario/CochesController.java
@@ -1,13 +1,12 @@
 package rey.angel.ProyectoFinal_Concesionario;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Optional;
-
+import java.util.List;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextInputDialog;
-import javafx.stage.StageStyle;
 import rey.angel.ProyectoFinal_Concesionario.model.Dao.CocheDao;
 import rey.angel.ProyectoFinal_Concesionario.model.DataObject.Coche;
 
@@ -17,6 +16,71 @@ public class CochesController {
 	
 	@FXML
 	private TableView<Coche> tab;
+	@FXML
+	private TableColumn<Coche, String> Matricula;
+	@FXML
+	private TableColumn<Coche, String> Marca;
+	@FXML
+	private TableColumn<Coche, String> Modelo;
+	@FXML
+	private TableColumn<Coche, String> Ano;
+	@FXML
+	private TableColumn<Coche, String> Color;
+	@FXML
+	private TableColumn<Coche, String> Kms;
+	@FXML
+	private TableColumn<Coche, String> Precio;
+	
+	@FXML
+	protected void initialize() {
+		this.configureTabla();
+		List<Coche> misCoches = (List<Coche>) cd.getAll();
+		tab.setItems(FXCollections.observableArrayList(misCoches));
+	}
+	
+	private void configureTabla() {
+		Matricula.setCellValueFactory(coche -> {
+			SimpleStringProperty ssp = new SimpleStringProperty();
+			ssp.setValue(coche.getValue().getMatricula());
+			return ssp;
+		});
+		
+		Marca.setCellValueFactory(coche -> {
+			SimpleStringProperty ssp = new SimpleStringProperty();
+			ssp.setValue(coche.getValue().getMarca());
+			return ssp;
+		});
+		
+		Modelo.setCellValueFactory(coche -> {
+			SimpleStringProperty ssp = new SimpleStringProperty();
+			ssp.setValue(coche.getValue().getModelo());
+			return ssp;
+		});
+		
+		Ano.setCellValueFactory(coche -> {
+			SimpleStringProperty ssp = new SimpleStringProperty();
+			ssp.setValue(coche.getValue().getAno());
+			return ssp;
+		});
+		
+		Color.setCellValueFactory(coche -> {
+			SimpleStringProperty ssp = new SimpleStringProperty();
+			ssp.setValue(coche.getValue().getColor());
+			return ssp;
+		});
+		
+		Kms.setCellValueFactory(coche -> {
+			SimpleStringProperty ssp = new SimpleStringProperty();
+			ssp.setValue(coche.getValue().getKilometros());
+			return ssp;
+		});
+		
+		Precio.setCellValueFactory(coche -> {
+			SimpleStringProperty ssp = new SimpleStringProperty();
+			ssp.setValue(coche.getValue().getPrecio());
+			return ssp;
+		});
+	}
 	
 	@FXML
     private void switchToInicio() throws IOException {
--- src/main/java/rey/angel/ProyectoFinal_Concesionario/InicioController.java
+++ src/main/java/rey/angel/ProyectoFinal_Concesionario/InicioController.java
@@ -2,8 +2,14 @@ package rey.angel.ProyectoFinal_Concesionario;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -19,7 +25,78 @@ public class InicioController {
 	@FXML
 	private TableView<Cliente> tab;
 	@FXML
-	private TableColumn<Cliente, String> id;
+	private TableColumn<Cliente, Integer> id;
+	@FXML
+	private TableColumn<Cliente, String> DNI;
+	@FXML
+	private TableColumn<Cliente, String> Nombre;
+	@FXML
+	private TableColumn<Cliente, String> Apellidos;
+	@FXML
+	private TableColumn<Cliente, String> Correo;
+	@FXML
+	private TableColumn<Cliente, String> Telefono;
+	@FXML
+	private TableColumn<Cliente, String> Direccion;
+	@FXML
+	private TableColumn<Cliente, String> Codigo_Postal;
+	
+	@FXML
+	protected void initialize() {
+		this.configuraTabla();
+		List<Cliente> misClientes = (List<Cliente>) cd.getAll();
+		tab.setItems(FXCollections.observableArrayList(misClientes));
+	}
+	
+	private void configuraTabla() {
+		id.setCellValueFactory(cliente ->{
+			ObservableValue<Integer> ov = new SimpleIntegerProperty().asObject();
+			((ObjectProperty<Integer>) ov).setValue(cliente.getValue().getId());
+			return ov;
+		});
+		
+		DNI.setCellValueFactory(cliente -> {
+			SimpleStringProperty ssp = new SimpleStringProperty();
+			ssp.setValue(cliente.getValue().getDni());
+			return ssp;
+		});
+		
+		Nombre.setCellValueFactory(cliente -> {
+			SimpleStringProperty ssp = new SimpleStringProperty();
+			ssp.setValue(cliente.getValue().getNombre());
+			return ssp;
+		});
+		
+		Apellidos.setCellValueFactory(cliente -> {
+			SimpleStringProperty ssp = new SimpleStringProperty();
+			ssp.setValue(cliente.getValue().getApellidos());
+			return ssp;
+		});
+		
+		Correo.setCellValueFactory(cliente -> {
+			SimpleStringProperty ssp = new SimpleStringProperty();
+			ssp.setValue(cliente.getValue().getCorreo());
+			return ssp;
+		});
+		
+		Telefono.setCellValueFactory(cliente -> {
+			SimpleStringProperty ssp = new SimpleStringProperty();
+			ssp.setValue(cliente.getValue().getTelefono());
+			return ssp;
+		});
+		
+		Direccion.setCellValueFactory(cliente -> {
+			SimpleStringProperty ssp = new SimpleStringProperty();
+			ssp.setValue(cliente.getValue().getDireccion());
+			return ssp;
+		});
+		
+		Codigo_Postal.setCellValueFactory(cliente -> {
+			SimpleStringProperty ssp = new SimpleStringProperty();
+			ssp.setValue(cliente.getValue().getCodigo_postal());
+			return ssp;
+		});
+	}
 
     @FXML
     private void switchToFormulario() throws IOException {
